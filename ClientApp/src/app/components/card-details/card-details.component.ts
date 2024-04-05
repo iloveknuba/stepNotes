b5ead6b780245f1720b3a06865f216f332d2b5e5
iloveknuba <xxxcccer982@gmail.com>
2024-04-05 13:38:30 +0300

    card-details.component.ts implement

--- ClientApp/src/app/components/card-details/card-details.component.ts
+++ ClientApp/src/app/components/card-details/card-details.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { Card } from '../../models/card.model';
 import {CardService} from "../../services/card.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-card-details',
@@ -12,17 +13,46 @@ import {CardService} from "../../services/card.service";
 export class CardDetailsComponent implements OnInit{
 
   @Input()
-  card?: Card ={
+  currentCard?: Card ={
     type: "card",
     title:'',
     text:'',
     tasks: [{text: '', completed: false}]
   };
-  constructor(private cardService: CardService) {}
+  constructor(private cardService: CardService,
+  private route: ActivatedRoute,
+  private router: Router) {}
     ngOnInit(): void {
-        throw new Error('Method not implemented.');
+        this.getCardDetails(this.route.snapshot.params["id"])
     }
 
-  deleteCard(card: Card) {}
-  updateCard(card: Card) {}
+    getCardDetails(id: string){
+    this.cardService.get(id)
+      .subscribe({
+        next: cardDetails => {
+          this.currentCard = cardDetails;
+          console.log(this.currentCard);
+        },
+        error: err => {console.log(err)}
+      })
+    }
+  deleteCard() {
+    this.cardService.delete(this.currentCard?.id)
+      .subscribe({
+        next: cardDetails => {
+          this.router.navigate(['/notes']);
+        },
+        error: err => {console.log(err)}
+      })
+  }
+  updateCard() {
+    this.cardService.update(this.currentCard?.id, this.currentCard)
+      .subscribe({
+        next: cardDetails => {
+          console.log(cardDetails);
+          this.router.navigate(['/notes'])
+        },
+        error: err => {console.log(err)}
+      })
+  }
 }
